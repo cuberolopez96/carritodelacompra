549a93f0d32ff3e3b2e4bdb7465fa77993c17e16
cuberolopez96 <cuberolopez96@gmail.com>
2016-06-11 22:31:42 +0200

    clase Pedidos creada,clase DetPedidos creada, Modelo Pedido creado , Modelo DetPedidos creado

--- includes/ModelDetallePedido.php
+++ includes/ModelDetallePedido.php
@@ -1,6 +1,6 @@
 <?php
 require_once "ConnectDB.php";
-require_once "Producto.php";
+require_once "DetPedido.php";
 class ModelProductos{
     private $conexion ="";
 
@@ -8,36 +8,37 @@ class ModelProductos{
         $this->conexion = new ConnectDB();
     }
 
-    public function add(Producto $producto){
-        $this->conexion->query("INSERT INTO productos(`descripcion`,`familia`,`pvp`)VALUES(:descripcion,:familia,:pvp)",array(
+    public function add(DetPedido $detpedido){
+        $this->conexion->query("INSERT INTO detpedidos(`idpedido`,`idProducto`,`unidades`,`importe`)VALUES(:idpedido,:idproducto,:unidades,:importe)",array(
 
-        ':descripcion'=>$producto->getDescripcion(),
+        ':idpedido'=>$detpedido->getIdPedido(),
 
-        ':familia'=>$producto->getFamilia(),
+        ':idproducto'=>$detpedido->getidProducto(),
 
-        ':pvp'=>$producto->getPvp(),
+        ':unidades'=>$detpedido->getUnidades(),
+        ':importe'=>$detpedido->getImporte(),
+
 
         ));
     }
     public function editarProducto($id,Producto $producto){
-         $this->conexion->query("INSERT INTO usuarios(`descripcion`,`familia`,`pvp`)VALUES(:descripcion,:familia,:pvp) WHERE id=:id",array(
-        ':id'=>$id,
-        ':descripcion'=>$producto->getDescripcion(),
-        ':familia'=>$producto->getFamilia(),
+         $this->conexion->query("INSERT INTO detpedidos(`idpedido`,`idProducto`,`unidades`,`importe`)VALUES(:idpedido,:idproducto,:unidades,:importe) WHERE id=:id",array(
 
-        ':pvp'=>$producto->getPvp(),
+         ':idpedido'=>$detpedido->getIdPedido(),
+
+         ':idproducto'=>$detpedido->getidProducto(),
+
+         ':unidades'=>$detpedido->getUnidades(),
+         ':importe'=>$detpedido->getImporte(),
 
 
-        ));
+         ));
     }
     public function buscarPorId($id){
-        return $this->conexion->query("SELECT * FROM productos WHERE id=:id",array(':id'=>$id))[0];
+        return $this->conexion->query("SELECT * FROM detpedidos WHERE id=:id",array(':id'=>$id))[0];
     }
     public function mostrar(){
-        return $this->conexion->query("SELECT * FROM productos");
-    }
-    public function login($email,$contraseña){
-        return $this->conexion->query("SELECT * FROM alumnos WHERE username=:email and password=:psw",array(':email'=>$email,':psw'=>$contraseña));
+        return $this->conexion->query("SELECT * FROM detpedidos");
     }
 }
 ?>
