d7e050e5fb53906ef97547a7bf1c0fe83a02cba1
cuberolopez96 <cuberolopez96@gmail.com>
2016-06-12 14:15:50 +0200

    register pedidos

--- modules/registerpedidos.php
+++ modules/registerpedidos.php
@@ -1,16 +1,17 @@
 <?php
-if(isset($_POST['add'])&&isset($_POST['descripcion'])&&isset($_POST['familia'])&&isset($_POST['pvp'])){
-    $producto = new Producto($_POST['descripcion'],$_POST['familia'],$_POST['pvp']);
-    $modelProductos->add($producto);
+$producto = $modelProductos->buscarPorId($_GET['id']);
+if(isset($_POST['add'])&&isset($_POST['unidades'])&&isset($_POST['importe'])){
+
+
 
 }
 ?>
 <div id="formulario">
     <form action=<?php echo $_SERVER['PHP_SELF'].'?opcion=gestionproductos&tipogestion=add'; ?> method="post">
-        <div class="form-group col-xs-6"><label for="">descripcion</label><input type="text" class="form-control col-xs-6" name="descripcion"></div>
-        <div class="form-group col-xs-6"><label for="">familia</label><input type="text" class="form-control" name="familia"></div>
 
-        <div class="form-group col-xs-6"><label for="">PVP</label><input type="text" class="form-control " name="pvp"></div>
+        <div class="form-group col-xs-6"><label for="">unidades</label><input type="text" class="form-control" name="familia"></div>
+
+        <div class="form-group col-xs-6"><label for="">importe</label><input type="text" class="form-control " name="importe" value=<?php echo $producto['importe']." X unidades"?>></div>
 
         <input type="submit" class="btn btn-default" name="add" value="añadir">
     </form>
