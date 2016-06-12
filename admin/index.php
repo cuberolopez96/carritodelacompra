cd813c09b08caf3a7a65f9ef30fd6266d3a760ab
cuberolopez96 <cuberolopez96@gmail.com>
2016-06-12 13:52:15 +0200

    modificado

--- admin/index.php
+++ admin/index.php
@@ -2,9 +2,11 @@
 require_once"../includes/ModelUsuarios.php";
 require_once"../includes/ModelProductos.php";
 require_once"../includes/ModelPedidos.php";
+require_once"../includes/ModelDetallePedido.php";
 $modelUsuarios = new ModelUsuarios();
 $modelProductos = new ModelProductos();
 $modelPedidos = new ModelPedidos();
+$modeldetallespedidos = new ModelDetallePedido();
 ?>
 <!DOCTYPE html>
 <html lang="en">
